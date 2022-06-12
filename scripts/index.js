4fe8680ee66eceddaff5db21c5049ef4f3cfd60b
Alexander Masaev <a.masaev-npo@yandex.ru>
2022-06-12 19:36:26 +0200

    3 steps of project done

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,95 @@
 const editButton = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
-const closePopupButton = document.querySelector('.popup__close-button');
-const formElement = document.querySelector('.popup__form');
-const nameInput = formElement.querySelector('.popup__field_type_name');
-const jobInput = formElement.querySelector('.popup__field_type_job');
+const addButton = document.querySelector('.profile__add-button');
+const popup = document.querySelectorAll('.popup');
+const popupEditProfile = document.querySelector('.popup_type_edit-profile');
+const popupAddCard = document.querySelector('.popup_type_add-card');
+const closePopupButton = document.querySelectorAll('.popup__close-button');
+const editFormElement = document.querySelector('.popup__form_type_edit-profile');
+const nameInput = editFormElement.querySelector('.popup__field_type_name');
+const jobInput = editFormElement.querySelector('.popup__field_type_job');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
+const addCardFormElement = document.querySelector('.popup__form_type_add-card');
+const placeName = addCardFormElement.querySelector('.popup__field_type_name');
+const placeImageUrl = addCardFormElement.querySelector('.popup__field_type_job');
 
+const cardTemplate = document.querySelector('#card').content;
+const elementsSection = document.querySelector('.elements');
 
-function popupDisplaySwitcher () {
-  if (!popup.classList.contains('popup_opened')) {
+const initialCards = [
+  {
+    name: 'Эльбрус',
+    link: 'images/elbrus-element.jpeg'
+  },
+  {
+    name: 'Нижний Новгород',
+    link: 'images/nizhniy-novgorod-element.jpeg'
+  },
+  {
+    name: 'Калуга',
+    link: 'images/kaluga-element.jpeg'
+  },
+  {
+    name: 'Омск',
+    link: 'images/omsk-element.jpeg'
+  },
+  {
+    name: 'Оймякон',
+    link: 'images/oymakon-element.jpeg'
+  },
+  {
+    name: 'Сочи',
+    link: 'images/sochi-element.jpeg'
+  },
+]
+
+function addCard(card) {
+  const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+  cardElement.querySelector('.element__image').style = `background: url(${card.link}) center / cover`;
+  cardElement.querySelector('.element__caption').textContent = card.name;
+  elementsSection.insertBefore(cardElement, elementsSection.firstChild);
+}
+
+initialCards.forEach(addCard);
+
+function popupEditProfileOpen() {
+  if (!popupEditProfile.classList.contains('popup_opened')) {
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
   }
 
-  popup.classList.toggle('popup_opened');
+  popupEditProfile.classList.add('popup_opened');
 }
 
-function formSubmitHandler (evt) {
+function editFormSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
 
-  popupDisplaySwitcher();
+  popupClose();
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
-editButton.addEventListener('click', popupDisplaySwitcher);
-closePopupButton.addEventListener('click', popupDisplaySwitcher);
+function addFormSubmitHandler(evt) {
+  evt.preventDefault();
+  let card = {};
+  card.name = placeName.value;
+  card.link = placeImageUrl.value;
+  addCard(card);
+
+  popupClose();
+}
+
+function popupAddCardOpen() {
+  popupAddCard.classList.add('popup_opened');
+}
+
+function popupClose() {
+  popup.forEach((i) => { i.classList.remove('popup_opened') });
+}
+
+editFormElement.addEventListener('submit', editFormSubmitHandler);
+addCardFormElement.addEventListener('submit', addFormSubmitHandler);
+editButton.addEventListener('click', popupEditProfileOpen);
+addButton.addEventListener('click', popupAddCardOpen);
+
+closePopupButton.forEach((i) => i.addEventListener('click', popupClose));
